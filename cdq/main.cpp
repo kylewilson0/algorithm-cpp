e4838c60d8d5bb1045cb2b1785af09f85d6ac6ec
kyle <kyle@kyle.local>
2026-07-03 17:30:32 +0800

    cdq

--- cdq/main.cpp
+++ cdq/main.cpp
@@ -1,48 +1,96 @@
 #include <iostream>
+#include <algorithm>
+#include <array>
+#include <vector>
+#define LOCAL
+using namespace std;
 
-#define MAX_N 200001
-#define MAX_M 3001
+struct BIT {
+    vector<int> bit;
+    int size;
 
-struct Line {
-    int l, r, v;
+    BIT(int n) : size(n + 1) {
+        bit.resize(size);
+    }
+
+    void update(int i, int x) {
+        for (; i < size; i += i & -i) bit[i] += x;
+    }
+
+    int query(int i) {
+        int ret = 0;
+        for (; i; i -= i & -i) ret += bit[i];
+        return ret;
+    }
 };
 
 struct Node {
-    int op, l, r, v;
+    int a, b, c, cnt, res;
+
+    bool operator!=(const Node &other) const {
+        if (a != other.a || b != other.b || c != other.c) return true;
+        return false;
+    }
 };
-Line line[MAX_N];
-int n, m, last_ans = 0;
 
-void init() {
-    scanf("%d %m", &n, &m);
-    for (int i = 0; i < n; ++i) {
-        scanf("%d %d %d", &line[i].l, &line[i].r, &line[i].v);
-    }
-
-    for (int i = n; i < n + m; ++i) {
-        int op;
-        scanf("%d", &op);
-        if (op == 1) {
-            scanf("%d %d %d", &line[i].l, &line[i].r, &line[i].v);
-        } else {
-            scanf("%d %d", &line[i].l, &line[i].r);
-        }
-    }
+bool compA(const Node &x, const Node &y) {
+    if (x.a != y.a)return x.a < y.a;
+    if (x.b != y.b)return x.b < y.b;
+    return x.c < y.c;
 }
 
+bool compB(const Node &x, const Node &y) {
+    if (x.b != y.b)return x.b < y.b;
+    return x.c < y.c;
+}
+
+constexpr int MAXN = 1e5 + 10;
+constexpr int MAXM = 2e5 + 10;
+
+int n, tot = 0;
+array<Node, MAXN> v{}, uv{};
+array<int, MAXN> ans{};
+BIT bit(MAXM);
+
 void cdq(int l, int r) {
-
     if (l == r) return;
-
-    int mid = (l + r) >> 1;
-    cdq(l, mid);
-    cdq(mid + 1, r);
-
+    int m = l + ((r - l) >> 1);
+    cdq(l, m);
+    cdq(m + 1, r);
+    sort(uv.begin() + l, uv.begin() + m + 1, compB);
+    sort(uv.begin() + m + 1, uv.begin() + r + 1, compB);
+    int j = l, i = m + 1;
+    while (i <= r) {
+        while (j <= m && uv[j].b <= uv[i].b) {
+            bit.update(uv[j].c, uv[j].cnt);
+            ++j;
+        }
+        uv[i].res += bit.query(uv[i].c);
+        ++i;
+    }
+    for (int k = l; k < j; ++k) bit.update(uv[k].c, -uv[k].cnt);
 }
 
 int main() {
-
-    freopen("../a.in", "r", stdin);
-
+#ifdef LOCAL
+    freopen("a.in", "r", stdin);
+#endif
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int k, t = 0;
+    cin >> n >> k;
+    for (int i = 1; i <= n; ++i) cin >> v[i].a >> v[i].b >> v[i].c;
+    sort(v.begin() + 1, v.begin() + n + 1, compA);
+    for (int i = 1; i <= n; ++i) {
+        ++t;
+        if (v[i] != v[i + 1]) {
+            ++tot;
+            uv[tot] = v[i];
+            uv[tot].cnt = t;
+            t = 0;
+        }
+    }
+    cdq(1, tot);
+    for (int i = 1; i <= tot; ++i) ans[uv[i].res + uv[i].cnt - 1] += uv[i].cnt;
+    for (int i = 0; i < n; ++i) cout << ans[i] << '\n';
     return 0;
 }
